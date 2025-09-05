d09cc005645f0bfd92befb9a0cec8e222d31eaa8
Oscar González <oscargonzalezpena@gmail.com>
2025-09-05 20:41:01 +0100

    feat: add Home page layout and content.
    - Create landing page using custom components.

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,8 +1,19 @@
+import StackedCards from "@/components/StackedCards"
+import Button from "@/components/Button"
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center">
-      Home Page
+    <div className="grid h-full w-full grid-cols-1 gap-10 md:grid-cols-2">
+      <div className="flex max-w-[70ch] flex-col items-center justify-center space-y-6 text-center md:items-start md:text-left">
+        <h1 className="font-montserrat text-5xl leading-tight font-bold text-balance">Find your perfect credit card</h1>
+        <p className="text-foreground-muted space-y-4 text-xl text-balance">
+          Check your eligibility and find the best offers available to you from a wide range of credit cards.
+        </p>
+        <Button variant="primary" className="w-fit">
+          Check eligibility now
+        </Button>
+      </div>
+      <StackedCards />
     </div>
   )
 }
