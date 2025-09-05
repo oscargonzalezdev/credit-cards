44183c11235f822ce38568cd370f990f151dfaf9
Oscar González <oscargonzalezpena@gmail.com>
2025-09-05 19:52:42 +0100

    feat: add StackedCards component with isometric card images.
    - Add three credit card images in PNG format.
    - Create StackedCards component to display images as a hero element.
    - Add smooth animation when mounting and hovering over the hero element.

---
+++ src/components/StackedCards.tsx
@@ -0,0 +1,33 @@
+import Image from "next/image"
+import { cn } from "@/utils/cn"
+
+export default function StackedCards() {
+  const cards = ["/images/isometric_card_1.png", "/images/isometric_card_2.png", "/images/isometric_card_3.png"]
+  return (
+    <div className="animate-fade-down group relative h-full min-h-[22rem] w-full">
+      {cards.map((card, index) => (
+        <div
+          key={index}
+          className={cn(
+            "absolute inset-0 m-auto h-fit w-fit transition-all duration-500 ease-out",
+            index === 0 &&
+              "perspective-distant group-hover:translate-y-4 group-hover:-rotate-8",
+            index === 1 &&
+              "-translate-x-2 translate-y-2 perspective-near group-hover:translate-y-0 group-hover:-rotate-2",
+            index === 2 &&
+              "perspective-dramatic group-hover:-translate-y-8 group-hover:rotate-6"
+          )}
+        >
+          <Image
+            src={card}
+            alt="Credit Card Image"
+            width={500}
+            height={325}
+            className="h-full w-full object-cover object-center"
+            priority
+          />
+        </div>
+      ))}
+    </div>
+  )
+}
