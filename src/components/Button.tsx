6874b46b212ab6d42e163acc178af11b8c7616a9
Oscar González <oscargonzalezpena@gmail.com>
2025-09-06 23:15:53 +0100

    refactor: enhance Button component with additional variant and improved styling.
    - Add the 'link' variant to create buttons that look like links.
    - Refactor button styles for better responsiveness and alignment.
    - Update props destructuring for cleaner code.

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,22 +1,30 @@
 import { cn } from "@/utils/cn"
 
-export default function Button(
-  props: React.ButtonHTMLAttributes<HTMLButtonElement> & { variant?: "primary" | "secondary" | "outline" }
-) {
+export type ButtonVariant = "primary" | "secondary" | "outline" | "link"
+
+export default function Button({
+  variant = "secondary",
+  className,
+  children,
+  ...props
+}: React.ButtonHTMLAttributes<HTMLButtonElement> & { variant?: ButtonVariant }) {
   const buttonClass = cn(
-    "rounded-full px-8 py-4 text-base font-semibold text-nowrap cursor-pointer font-montserrat transition-all duration-300 disabled:opacity-70 disabled:cursor-not-allowed leading-tight",
-    props.className,
-    props.variant === "primary" &&
-      "bg-primary text-primary-foreground hover:bg-primary-muted hover:text-primary-muted-foreground focus:bg-primary-muted focus:text-primary-muted-foreground",
-    props.variant === "secondary" &&
-      "bg-secondary text-secondary-foreground hover:bg-secondary-muted hover:text-secondary-muted-foreground focus:bg-secondary-muted focus:text-secondary-muted-foreground",
-    props.variant === "outline" &&
-      "bg-transparent text-foreground outline outline-2 outline-foreground -outline-offset-2 hover:bg-secondary/10 focus:bg-secondary/10"
+    "transition-all duration-300 disabled:opacity-70 font-montserrat text-base font-semibold text-nowrap cursor-pointer flex items-center gap-2",
+    variant === "link"
+      ? "text-secondary hover:text-secondary-muted-foreground"
+      : "rounded-full px-8 py-4 text-nowrap text-center disabled:cursor-not-allowed justify-center leading-tight",
+    variant === "primary" &&
+      "bg-primary text-primary-foreground hover:bg-primary-muted hover:text-primary-muted-foreground",
+    variant === "secondary" &&
+      "bg-secondary text-secondary-foreground hover:bg-secondary-muted hover:text-secondary-muted-foreground",
+    variant === "outline" &&
+      "bg-transparent text-foreground outline outline-2 outline-foreground -outline-offset-2 hover:bg-secondary/10",
+    className
   )
 
   return (
-    <button className={buttonClass}>
-      {props.children}
+    <button className={buttonClass} {...props}>
+      {children}
     </button>
   )
 }
