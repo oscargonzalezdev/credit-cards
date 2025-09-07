64e4a81acd879de7e27a2f92d0e6800174b469fc
Oscar González <oscargonzalezpena@gmail.com>
2025-09-08 00:04:05 +0100

    refactor: update Home page to remove modal and integrate link to the credit cards page .
    - Clean up unsed modal functionaly.
    - Replace button with a link to simplify logic.

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,37 +1,25 @@
 "use client"
 
 import StackedCards from "@/components/StackedCards"
-import Button from "@/components/Button"
-import CustomerLoginModal from "@/components/CustomerLoginModal"
-import { useState } from "react"
 import { cn } from "@/utils/cn"
+import Link from "next/link"
 
 export default function Home() {
-  const [isModalOpen, setIsModalOpen] = useState(false)
-
-  const handleOpenModal = () => {
-    setIsModalOpen(true)
-  }
-
-  const handleCloseModal = () => {
-    setIsModalOpen(false)
-  }
 
   return (
     <div className="h-full">
-      <div className={cn("grid h-full grid-cols-1 gap-10 md:grid-cols-2", isModalOpen && "pointer-events-none")}>
+      <div className={cn("grid h-full grid-cols-1 gap-10 md:grid-cols-2")}>
         <div className="flex max-w-[70ch] flex-col items-center justify-center space-y-6 text-center md:items-start md:text-left">
           <h1 className="font-montserrat text-5xl leading-tight font-bold text-balance">Find your perfect credit card</h1>
           <p className="text-foreground-muted space-y-4 text-xl text-balance">
             Check your eligibility and find the best offers available to you from a wide range of credit cards.
           </p>
-          <Button variant="primary" onClick={handleOpenModal}>
+          <Link href="/credit-cards" className="as-button">
             Check eligibility now
-          </Button>
+          </Link>
         </div>
         <StackedCards />
       </div>
-      <CustomerLoginModal isOpen={isModalOpen} onClose={handleCloseModal} />
     </div>
   )
 }
